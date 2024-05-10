d3beebea2f31856261f1274ed06ee1dcb52e8293
Bernardess7 <bernardesrocha7@gmail.com>
2024-05-10 16:48:43 -0300

    ajuste estrutura html

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent default form submission
+
+ 
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if (username === "user" && password === "password") {
+    
+        window.location.href = "dashboard.html";
+    } else {
+      
+        document.getElementById("message").textContent = "tudo bem";
+    }
+});
 
